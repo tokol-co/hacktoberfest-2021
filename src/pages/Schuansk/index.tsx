cec3511a07874306c539012ac39d653b69e21a25
schuansk <schuansk@hotmail.com.br>
2021-10-06 08:55:03 -0300

    Profile styling

--- src/pages/Schuansk/index.tsx
+++ src/pages/Schuansk/index.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 import Header from '../../components/Header';
 
 import {
-  Container
+  Container,
+  Profile,
 } from './style';
 
 const Schuansk = () => {
   return (
     <Container>
       <Header />
-      <header>
+      <Profile>
         <img src="https://avatars.githubusercontent.com/u/11741138?s=400&u=2d6824fb3b0b8fae51e8738f5d29828069712b66&v=4" alt="Schuansk photo" />
         <a href="https://github.com/schuansk" target="_blank">Schuansk Torres</a>
-      </header>
+      </Profile>
       <main>
         <section>
           <header>
--- src/pages/Schuansk/style.ts
+++ src/pages/Schuansk/style.ts
@@ -6,3 +6,30 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const Profile = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  img {
+    width: 200px;
+    border-radius: 100px;
+    border: 2px solid #F74700;
+  }
+
+  a {
+    margin-top: 10px;
+    color: #FFFFFF;
+    text-decoration: none;
+    font-size: 26pt;
+    font-weight: bold;
+
+    transition: all .2s;
+
+    &:hover {
+      color: #F74700;
+    }
+  }
+`;
