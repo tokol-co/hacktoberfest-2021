ebed4b1580d3ef858e42093e403d83f29e5effaa
Guilherme H. Santos <guilherme.hxsantos@gmail.com>
2021-10-06 00:37:29 -0300

    Added styles and profile picture

--- src/pages/Guilherme/index.tsx
+++ src/pages/Guilherme/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import Header from '../../components/Header';
+
+import {
+  Container,
+  ProfileImg,
+  Title,
+  Subtitle,
+  Content,
+} from './styles';
 
 const Guilherme = () => {
   return (
-    <h2>
-      Guilherme
-    </h2>
+    <Container>
+      <Header />
+      <ProfileImg src="https://avatars.githubusercontent.com/u/47790486?v=4" alt="Guilherme's photo" />
+      <h2>
+        Guilherme Santos
+      </h2>
+    </Container>
   )
 }
 
---
+++ src/pages/Guilherme/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProfileImg = styled.img`
+  margin: 20px 0;
+  width: 200px;
+  border: 2px solid #F74700;
+  border-radius: 50%;
+`;
+
+export const Title = styled.a`
+  margin: 10px 0;
+  font-size: 1.8rem;
+  font-weight: bold;
+  text-decoration: none;
+  color: #fff;
+
+  :hover {
+    transition: 0.3s;
+    color: #677662;
+  }
+`;
+
+export const Subtitle = styled.div`
+  margin: 10px 0;
+  font-size: 1.1rem;
+  color: #F74700;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.1rem;
+  width: 500px;
+  padding: 20px;
+
+  h2 {
+    margin: 10px 0;
+  }
+
+  p {
+    margin: 5px 0;
+  }
+
+  :hover {
+    transition: 0.3s;
+    color: #677662;
+  }
+`;
