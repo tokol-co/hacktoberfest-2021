e80e99254045c3c7ea4d6f8b48c20ca0bf948d43
schuansk <schuansk@hotmail.com.br>
2021-10-05 22:08:56 -0300

    Profile summary inclusion

--- src/pages/Schuansk/index.tsx
+++ src/pages/Schuansk/index.tsx
@@ -10,6 +10,19 @@ const Schuansk = () => {
         <img src="https://avatars.githubusercontent.com/u/11741138?s=400&u=2d6824fb3b0b8fae51e8738f5d29828069712b66&v=4" alt="Schuansk photo" />
         <a href="https://github.com/schuansk" target="_blank">Schuansk Torres</a>
       </header>
+      <main>
+        <section>
+          <header>
+            <p>Web developer | NodeJS | ReactJS </p>
+          </header>
+          <article>
+            <p>Hi, I'm a little grasshopper that seeks new knowledge every day.</p>
+            <p>I'm currently a junior developer working mainly with NodeJS and ReactJS, but I'm taking my first steps in Python, PHP and Java (Android).</p>
+            <p>I think no matter what our pace, what we can't do is stop, so we're going to keep coding and improving every day.</p>
+            <p>Let's go 🚀.</p>
+          </article>
+        </section>
+      </main>
     </>
   )
 }
