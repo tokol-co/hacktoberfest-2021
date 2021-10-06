bc334be39e091c679cb81a2b6ab8c5a186ed19ac
schuansk <schuansk@hotmail.com.br>
2021-10-05 21:55:21 -0300

    Header with profile name and image

--- src/pages/Schuansk/index.tsx
+++ src/pages/Schuansk/index.tsx
@@ -6,6 +6,10 @@ const Schuansk = () => {
   return (
     <>
       <Header />
+      <header>
+        <img src="https://avatars.githubusercontent.com/u/11741138?s=400&u=2d6824fb3b0b8fae51e8738f5d29828069712b66&v=4" alt="Schuansk photo" />
+        <a href="https://github.com/schuansk" target="_blank">Schuansk Torres</a>
+      </header>
     </>
   )
 }
